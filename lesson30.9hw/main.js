6f989e9c9aaeb48c8c04f84fd7f1c950ff0938b2
Aliia <imangazieva2001@icloud.com>
2024-10-03 14:12:20 +0200

    Add files via upload

---
+++ lesson30.9hw/main.js
@@ -0,0 +1,12 @@
+let username = "Aliia";
+let age = 23;
+let gender ="female";
+let country ="Germany";
+
+let result;
+if (age>18 && username|| gender!= "man" && country === "germany"){
+    result = true;
+}else {
+    result = false;
+}
+console.log(result);
