45f212de133d157a7cb34bf478773a21dd162e1c
Aliia <imangazieva2001@icloud.com>
2024-11-16 16:50:42 +0100

    Add files via upload

--- homework1.11/js/app.js
+++ homework1.11/js/app.js
@@ -1,4 +1,27 @@
-const originalArray = [5,10,15,20,25];
+// zadanie 1 
+const originalArray = [5, 10, 15, 20, 25];
 const newArray = originalArray.map(num => num + 3);
 
 console.log(newArray);
+
+// zadanie 2
+const words = ['Aplle', 'Banana', 'cherry', 'date'];
+const lengths = words.map(word => word.length);
+
+console.log(lengths);
+
+// zadanie 3
+const products = [
+
+    { name: "laptop", price: 1000 },
+    { name: "phone", price: 600 },
+    { name: "tablet", price: 450 }
+];
+ const discountedProducts = products.map(product =>{
+    return {
+        name:product.name,
+        price:(product.price *0.8).toFixed(2)
+    };
+ });
+
+ console.log(discountedProducts);
